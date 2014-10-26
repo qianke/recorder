240f50eeae448880cbdd0927d634c3457e7d5d52
yiqianke@163.com <yiqianke@163.com>
2014-10-27 00:17:10 +0800

    edit

--- src/com/example/recorder/CrimeListFragment.java
+++ src/com/example/recorder/CrimeListFragment.java
@@ -1,13 +1,31 @@
 package com.example.recorder;
 
+import java.util.ArrayList;
+
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CrimeListFragment extends ListFragment {
+	private ArrayList<Crime> mCrimes;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		CrimeLab.get(getActivity()).getCrimes();
+		mCrimes = CrimeLab.get(getActivity()).getCrimes();
+				
+		ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(), R.layout.simple_list_item, mCrimes);
+		setListAdapter(adapter);
+	}
+	
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		Crime c = (Crime) getListAdapter().getItem(position);
 	}
 }
